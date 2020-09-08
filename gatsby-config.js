5b5c424acd855c3e51d31bb367c2b19bfb85903f
Hannah Redmond <hredmond@Hannahs-MBP.broadband>
2020-09-08 09:49:20 +0100

    addition of google analytics - reference updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-177384334-1",
+        trackingId: "UA-177386647-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional
